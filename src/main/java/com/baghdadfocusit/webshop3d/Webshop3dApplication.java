067209e1c27ad5c00dc9eb3ee22d7549c75d0e7e
Ahmed Aziz <ahmed.aziz@ing.com>
2020-11-05 13:48:07 +0100

    add bucket4j to make only 10 calls within 30 sec for the orders endpoint

--- src/main/java/com/baghdadfocusit/webshop3d/Webshop3dApplication.java
+++ src/main/java/com/baghdadfocusit/webshop3d/Webshop3dApplication.java
@@ -2,6 +2,7 @@ package com.baghdadfocusit.webshop3d;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @PropertySources({@PropertySource("classpath:email.properties")})
 @EnableAsync
+@EnableCaching
 public class Webshop3dApplication {
 
     public static void main(String[] args) {
