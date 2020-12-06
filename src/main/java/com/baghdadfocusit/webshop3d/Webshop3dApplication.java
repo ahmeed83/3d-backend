1b85b8edcf25ee7dee54e5f56d1853ed28ac238c
Ahmed Aziz <ahmed.aziz@ing.com>
2020-12-06 23:03:09 +0100

    add config for prd deployement

--- src/main/java/com/baghdadfocusit/webshop3d/Webshop3dApplication.java
+++ src/main/java/com/baghdadfocusit/webshop3d/Webshop3dApplication.java
@@ -4,15 +4,12 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * SpringBootApplication main class.
  */
 @SpringBootApplication
-@PropertySources({@PropertySource("classpath:email.properties")})
 @EnableAsync
 @EnableCaching
 @EnableEncryptableProperties
