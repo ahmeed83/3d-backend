c5a03d1a68e33f93416bba4badf5848500d9106e
Ahmed Aziz <ahmed.aziz@ing.com>
2020-12-29 17:41:01 +0100

    add change currency to the project

---
+++ src/main/java/com/baghdadfocusit/webshop3d/controller/CurrencyController.java
@@ -0,0 +1,27 @@
+package com.baghdadfocusit.webshop3d.controller;
+
+import com.baghdadfocusit.webshop3d.service.CurrencyService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Currency controller.
+ */
+@RestController
+@RequestMapping("resources/currency")
+public class CurrencyController {
+
+    private final CurrencyService currencyService;
+
+    public CurrencyController(final CurrencyService currencyService) {
+        this.currencyService = currencyService;
+    }
+
+    @GetMapping
+    public ResponseEntity<String> getCurrencyPrice() {
+        return new ResponseEntity<>(currencyService.retrieveItem(), HttpStatus.OK);
+    }
+}
